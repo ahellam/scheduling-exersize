83c6de7c2f0450ba961838ff223ca36cbc718d86
Aaron Hellam <aaronhellam@gmail.com>
2022-06-14 16:41:31 -0700

    empl cntrl rspec works

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,11 +2,12 @@ class EmployeesController < ApplicationController
 
     def index
         if params[:sort_by] == "first_name"
-            render json: Employee.all.order(:first_name)
+            render json: Employee.all.order(:first_name) 
         elsif params[:sort_by] == "last_name"
-            render json: Employee.all.order(:last_name)
+            render json: Employee.all.order(:last_name) 
         else
-            render json: Employee.all.order(:first_name)
+            # if no params specified, default to sorting by first_name 
+            render json: Employee.all.order(:first_name) 
         end
     end
 
--- spec/requests/employees_spec.rb
+++ spec/requests/employees_spec.rb
@@ -1,7 +1,54 @@
 require 'rails_helper'
+require 'pry'
+# Request URL: http://localhost:3000/employees?sort_by=first_name
 
-RSpec.describe "Employees", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe :EmployeesController do 
+  before do
+    e1 = Employee.create!(first_name: "Andy", last_name: "Zamburg")
+    e2 = Employee.create!(first_name: "Zack", last_name: "Anderson")
+  end
+
+  describe "GET #index with first_name params" do
+
+    before do
+      get '/employees?sort_by=first_name'
+    end
+
+    # it do
+    # binding.pry
+    # end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns names in correct order" do 
+      expect(JSON.parse(response.body).first['name']).to eq("Andy Zamburg")
+    end
+  end
+
+  describe "GET #index with last_name params" do
+
+    before do
+      get '/employees?sort_by=last_name'
+    end
+
+    # it do
+    # binding.pry
+    # end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns names in correct order" do 
+      expect(JSON.parse(response.body).first['name']).to eq("Zack Anderson")
+    end
+
   end
 end
+
+
+
+
+
